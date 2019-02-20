a734655cd314b7be3606da91104a97bee07ce57a
louis2again <louis2again@hotmail.com>
2019-02-20 15:54:18 +0000

    Advanced Routing yelp-MVP created controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '01372-567876',
+    category:     'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0202-567876',
+    category:     'french'
+  },
+  {
+    name:         'Waggas',
+    address:      '10 Boundary St, London E2 7JE',
+    phone_number: '01562-567876',
+    category:     'chinese'
+  },
+  {
+    name:         'Nandos',
+    address:      '80 Shoreditch High St, London E1 6PQ',
+    phone_number: '0662-567876',
+    category:     'belgian'
+  },
+  {
+    name:         'Wimpy',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '01372-567876',
+    category:     'italian'
+  },
+  {
+    name:         'Taken',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0202-567876',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
