99374749cc4cbf47215aaf46269a53615fe90ade
louis2again <louis2again@hotmail.com>
2019-02-21 10:55:50 +0000

    Advanced Routing yelp-MVP incomplete bootstrap error

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,38 @@
 class RestaurantsController < ApplicationController
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+    @reviews = @restaurant.reviews
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :description, :stars)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/create'
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/create'
+  get 'restaurants/new'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:index, :new, :create]
